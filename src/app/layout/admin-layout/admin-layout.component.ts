06378c4ca564127939e956279d81b11f501d0384
ayanik <ayanik@uni-yaz.com>
2024-01-07 22:53:16 +0300

    RouterLink added

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AdminNavComponent } from "../../nav/admin-nav/admin-nav.component";
 @Component({
-  selector: 'app-admin-layout',
+  selector: "app-admin-layout",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './admin-layout.component.html',
-  styleUrl: './admin-layout.component.css'
+  imports: [RouterOutlet, AdminNavComponent],
+  templateUrl: "./admin-layout.component.html",
+  styleUrl: "./admin-layout.component.css",
 })
-export class AdminLayoutComponent {
-
-}
+export class AdminLayoutComponent {}
