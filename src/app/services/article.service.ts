154393e022dfd1f1796412777c3b479673231d9b
ayanik <ayanik@uni-yaz.com>
2024-01-12 22:43:15 +0300

    article service and models created

---
+++ src/app/services/article.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Article } from "../models/article";
+import { ArticlePg } from "../models/article-pg";
+import { tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ArticleService {
+  constructor(private httpClient: HttpClient) {}
+
+  private apiUrl: string = "http://localhost:5116/api/articles";
+  public loading: boolean = true;
+
+  getArticles(page: number, pageSize: number) {
+    let api = `${this.apiUrl}/${page}/${pageSize}`;
+
+    return this.httpClient.get<ArticlePg[]>(this.apiUrl).pipe(// pipe işlemde araya giren bir method türüdür.
+      tap((x) => {
+        this.loading = false;
+      })
+    );
+  }
+}
